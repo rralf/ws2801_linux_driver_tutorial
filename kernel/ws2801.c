ccab51ec7f0095de29680991f09f563376e5ae87
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2021-08-21 23:05:44 +0200

    Version 4 - Probe / Remove

--- kernel/ws2801.c
+++ kernel/ws2801.c
@@ -16,6 +16,16 @@
 
 #define DRIVER_NAME	"ws2801"
 
+static int ws2801_probe(struct platform_device *pdev)
+{
+	return -1;
+}
+
+static int ws2801_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
 static const struct of_device_id of_ws2801_match[] = {
 	{
 		.compatible = DRIVER_NAME,
@@ -26,6 +36,8 @@ static const struct of_device_id of_ws2801_match[] = {
 MODULE_DEVICE_TABLE(of, of_ws2801_match);
 
 static struct platform_driver ws2801_driver = {
+	.probe = ws2801_probe,
+	.remove = ws2801_remove,
 	.driver = {
 		.name = DRIVER_NAME,
 		.of_match_table = of_ws2801_match,
